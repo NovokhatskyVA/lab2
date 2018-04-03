ba6384033b887c3e0fbe5c5d55bba47745fddfd4
u211-07 <u211-07@public.mpei.local>
2018-04-03 12:05:06 +0300

    add file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Enter A and B";
+    cin>>a>>b;
+    cout<<"A+B="<<a+b<<"\n";
+    cout<<"2a"<<a*2<<"\n";
+    cout<<"a-b"<<a-b<<"\n";
+    cout<<"a*b"<<a*b<<"\n";
+    cout<<"a/b"<<a/b<<"\n";
+
+}
